1a804494b12c47ca2561e62422f4797a200d0c8c
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-01-20 14:26:21 +0000

    update UnitTests.py

--- pyGenicParser/Tests/UnitTests.py
+++ pyGenicParser/Tests/UnitTests.py
@@ -17,11 +17,14 @@ class MyTestCase(unittest.TestCase):
         assert Path(Path(__file__).parent, "Data", "Write", "write.txt").exists()
 
     @staticmethod
-    def test_bgen_bgi_write():
+    def _loader():
+        """Call the bgen file"""
+        return BgenObject(Path(Path(__file__).parent, "Data", "EUR.ldpred_21.bgen"))
+
+    def test_bgen_bgi_write(self):
         """Test writing bgen.bgi"""
-        bgen_path = Path(Path(__file__).parent, "Data", "EUR.ldpred_21.bgen")
         write_path = Path(Path(__file__).parent, "Data", "Write")
-        BgenObject(bgen_path).create_bgi(write_path)
+        self._loader().create_bgi(write_path)
 
         out_path = Path(Path(__file__).parent, "Data", "Write", "EUR.ldpred_21.bgen.bgi")
         assert out_path.exists()
@@ -37,11 +40,6 @@ class MyTestCase(unittest.TestCase):
         out_path = Path(Path(__file__).parent, "Data", "Write", "EUR.ldpred_21.bim.bgi")
         assert out_path.exists()
         out_path.unlink()
-
-    @staticmethod
-    def _loader():
-        """Call the bgen file"""
-        return BgenObject(Path(Path(__file__).parent, "Data", "EUR.ldpred_21.bgen"))
 
     def test_stats(self):
         """
@@ -69,7 +67,6 @@ class MyTestCase(unittest.TestCase):
     def test_extractors(self):
         """Test all extractors work based on our three if elif else statements of len(snps) > 1, ==1, 0"""
         bgen = self._loader()
-
         self.assertEqual(bgen.sid_count, 7909)
 
         snps = ['rs55776382', 'rs2801301']
@@ -85,9 +82,47 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(len(bgen.dosage_from_sid(snps)), 1)
 
         snps = []
+        print("Should return 3 no names passed")
         self.assertEqual(len(bgen.info_from_sid(snps)), 0)
         self.assertEqual(len(bgen.variant_from_sid(snps)), 0)
         self.assertEqual(len(bgen.dosage_from_sid(snps)), 0)
+
+    def test_indexing(self):
+        """Tests if indexing on snp or iid actually produces an indexed request"""
+        bgen = self._loader()
+        sid_array = ['rs55776382', 'rs2801301', 'rs3869758', 'rs79913394', 'rs35829851', 'rs118189563']
+
+        self.assertEqual(bgen[:, [1, 2, 3]].sid_count, 3)
+        self.assertEqual(bgen[[1, 2, 3], :].iid_count, 3)
+        self.assertEqual(bgen[:, :3].sid_count, 3)
+        self.assertEqual(bgen[:3, :].iid_count, 3)
+        self.assertEqual(bgen[:, bgen.sid_to_index(sid_array)].sid_count, 6)
+
+        # Check we still get the right length even with return false (returns -1 if we fail to find something)
+        bgen = bgen[:, bgen.sid_to_index(sid_array[:3])]
+        self.assertEqual(bgen[:, bgen.sid_to_index(sid_array, True)].sid_count, 3)
+
+        # Now do the same with iid
+        iid_list = [[0, 0], [1, 1], [2, 2]]
+        bgen = bgen[bgen.iid_to_index(iid_list), :]
+        self.assertEqual(bgen.iid_count, 3)
+
+    def test_indexed_data(self):
+
+        bgen = self._loader()
+        sid_array = ['rs55776382', 'rs2801301', 'rs3869758', 'rs79913394', 'rs35829851', 'rs118189563']
+
+        bgen = bgen[:, bgen.sid_to_index(sid_array[:3])]
+
+        self.assertEqual(len(bgen.sid_array()), 3)
+        self.assertEqual(len(bgen.info_array()), 3)
+        self.assertEqual(len(bgen.dosage_array()), 3)
+        self.assertEqual(len(bgen.variant_array()), 3)
+
+        # Check that snp names > than length of bgen.sid_count don't cause a crash
+        self.assertEqual(len(bgen.info_from_sid(sid_array)), 3)
+        self.assertEqual(len(bgen.dosage_from_sid(sid_array)), 3)
+        self.assertEqual(len(bgen.variant_from_sid(sid_array)), 3)
 
 
 if __name__ == '__main__':
